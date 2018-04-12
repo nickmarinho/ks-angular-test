c5784d494f1d084c100b2a891c3490f950004a26
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-04-12 16:40:50 -0300

    Creating Observable of getUsers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UsersService } from './service/users.service';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    HttpClientModule
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [
     UsersService
   ],
--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,22 +1,27 @@
-import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Users } from '../model/users.model';
+import { Component, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class UsersService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: Http) {
+    let obj;
+    this.getUsers().subscribe(data => obj = data, error => console.log(error));
+ }
 
   /**
   * get user list
   */
-  getUsers() {
-    return this.http.get(`users.json`)
-    .subscribe(
-      data => {
-        return data;
-      }
-    );
-  }
+  public getUsers(): Observable<Users> {
+    return this.http
+      .get('users.json')
+      .map((res: any) => res.json());
+   }
 }
