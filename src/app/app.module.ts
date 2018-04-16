ac3935327d42db452e3bf9b0aced4b9e153326aa
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-04-15 21:10:20 -0300

    Cleaning modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { HttpClientModule } from '@angular/common/http';
 import { UsersService } from './service/users.service';
-import { Http, Headers, ConnectionBackend, RequestOptions } from '@angular/http';
 import { UserFormComponent } from './forms/user-form.component';
+import { FormsModule, FormGroup } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,10 +15,11 @@ import { UserFormComponent } from './forms/user-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    FormGroup,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot(),
-    FormsModule
+    ModalModule.forRoot()
   ],
   exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [
