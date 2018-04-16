fb51a78a22b281baa664ae951b858674c10bfab6
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-04-16 15:12:45 -0300

    Fix to list users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { UsersService } from './service/users.service';
 import { Users } from './model/users.model';
 
@@ -9,12 +9,25 @@ import { Users } from './model/users.model';
     '../../node_modules/bootstrap/dist/css/bootstrap.min.css',
     './app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges {
   constructor(private usersService: UsersService) { }
 
-  users = JSON.parse(
-    JSON.parse(
-      this.usersService.getUsers()
-    )
-  );
+  users;
+
+  ngOnInit() {
+    this.users = JSON.parse(
+      JSON.parse(
+        this.usersService.getUsers()
+      )
+    );
+  }
+
+  ngOnChanges() {
+    this.users = JSON.parse(
+      JSON.parse(
+        this.usersService.getUsers()
+      )
+    );
+  }
+
 }
