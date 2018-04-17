0574cf79584f5b761ad57e58aa9dad79f187f7bc
LUCIANO MARINHO <nickmarinho@gmail.com>
2018-04-16 23:34:50 -0300

    Adding h1 title to the component. Updating tests.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ks-angular-test1 App', () => {
+describe('ks-angular-test App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('ks-angular-test1 App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('User List');
   });
 });
